c05f22029e38f99c292359c36f34de7ee1e15b2b
iitenkie <846708308gm@gmail.com>
2024-03-02 13:42:35 +0800

    fix types

--- types/util.d.ts
+++
@@ -1,7 +0,0 @@
-import { Size, TextLanguage } from "./types/shared";
-import { BoundingBox } from "opentype.js";
-export declare function bulkSetAttributes(element: Element, attributes: Record<string, string | number>): Element;
-export declare function calcGlyphSize(boundingBox: BoundingBox): Size;
-export declare function isSpace(text: string): boolean;
-export declare function determineLanguage(text: string): TextLanguage;
-export declare function isBrowser(): boolean;
